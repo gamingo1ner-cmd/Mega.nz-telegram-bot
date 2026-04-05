7ca45360acd9037323e407339d81aa8569d462f8
gamingo1ner-cmd <gaming.o1ner@gmail.com>
2026-04-05 20:53:11 +0530

    Create logging_config.py

---
+++ logging_config.py
@@ -0,0 +1,11 @@
+import logging
+
+def setup_logging():
+
+    logging.basicConfig(
+        filename="logs/bot.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    logging.info("Logging started")
